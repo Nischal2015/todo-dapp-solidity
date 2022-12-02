ddda493830c4aa3b90e9c9abe37d2d9740581615
Shrestha33 <milanshrestha373@gmail.com>
2022-12-02 16:35:40 +0545

    Hardhat configured

---
+++ hardhat.config.ts
@@ -0,0 +1,27 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY!],
+      chainId: 5,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "./gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
+};
+
+export default config;
